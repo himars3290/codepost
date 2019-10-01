8e3914d27efe9dc4aeed71ceee32abe4259d2bc3
rashmi <rashmi.manandhar7@gmail.com>
2019-10-01 16:31:29 -0400

    create a service for accessing posts and a nav bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {PostService} from './service/post.service';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [PostService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/service/post.service.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
+import {map} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+  result: any;
+
+  constructor(private _http: Http) {
+  }
+
+  getPosts() {
+    return this._http.get('/api/posts')
+      .pipe(
+        map(result => this.result = result.json())
+      );
+  }
+}
